35e583b88bdaeb8cde3a9e381324c10484472da4
MisterGuinness <guinness.meredith@gmail.com>
2021-10-23 23:16:01 +0800

    Fixed Issue #49. Pass directory parameters to configure

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 test -n "$srcdir" || srcdir=$(dirname "$0")
 test -n "$srcdir" || srcdir=.
@@ -14,18 +14,43 @@ cd $srcdir
 
 PKG_NAME=$(autoconf --trace 'AC_INIT:$1' configure.ac)
 
-if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
-        echo "*** WARNING: I am going to run 'configure' with no arguments." >&2
-        echo "*** If you wish to pass any to it, please specify them on the" >&2
-        echo "*** '$0' command line." >&2
-        echo "" >&2
+# default to local (per-user) installation
+mode="local"
+
+# switch to machine-wide installation only if first parameter specifies that
+if [ $# -eq 1 ]; then
+  if [ "$1" = "machine" ]; then
+    mode="$1"
+  fi
+fi
+
+# set the directories for configure according to the mode specified
+# for a local installation, locale+schemas+icons directories are under the
+# extension's directory but for machine-wide, those directories are separate
+if [ "$mode" = "local" ]; then
+  echo "*** NOTE: local installation will be configured." >&2
+  #
+  # note: (HOME) causes an error, but {HOME} works fine
+  params='--prefix=${HOME}/.local'
+  params+=' --with-extension-dir=$(datarootdir)/gnome-shell/extensions/$(uuid)'
+  params+=' --with-locale-dir=$(extensiondir)/locale'
+  params+=' --with-schema-dir=$(extensiondir)/schemas'
+  params+=' --with-icon-dir=$(extensiondir)/icons'
+else
+  echo "*** NOTE: machine wide installation will be configured." >&2
+  params='--prefix=/usr'
+  params+=' --with-extension-dir=$(datarootdir)/gnome-shell/extensions/$(uuid)'
+  params+=' --with-locale-dir=$(datarootdir)/locale'
+  params+=' --with-schema-dir=$(datarootdir)glib-2.0/schemas'
+  params+=' --with-icon-dir=$(datarootdir)/icons'
 fi
 
 autoreconf --verbose --force --install || exit 1
 
 cd "$olddir"
+
 if [ "$NOCONFIGURE" = "" ]; then
-        $srcdir/configure "$@" || exit 1
+        $srcdir/configure $params || exit 1
 
         if [ "$1" = "--help" ]; then
                 exit 0
